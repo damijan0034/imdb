bade17358d48f6883b5e921715198b310f886a19
damijan0034 <damijan0034@gmail.com>
2023-03-04 03:36:26 -0800

    add mounted

--- src/components/DarkmodeSwitch.jsx
+++ src/components/DarkmodeSwitch.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {MdLightMode} from 'react-icons/md'
 import {MdDarkMode} from 'react-icons/md'
 import { useTheme } from 'next-themes'
@@ -8,12 +8,17 @@ import { useTheme } from 'next-themes'
 export default function DarkmodeSwitch() {
 
   const{systemTheme,theme,setTheme}=useTheme()
+
+  const[mounted,setMounted]=useState(false)
+  useEffect(()=>setMounted(true),[])
+
   const currentTheme =theme === 'system' ? systemTheme : theme
 
   return ( 
     <div className='flex'>
 
-      {currentTheme ==='dark' ?(
+      {mounted &&
+      currentTheme ==='dark' ?(
         <MdLightMode className='cursor-pointer hover:text-amber-500' onClick={()=>setTheme("light")} />
       ):
        (<MdDarkMode className='cursor-pointer hover:text-amber-500' onClick={()=>setTheme("dark")} />)
