a58048a3168895239d5806a99c5e2ceb074b5d61
damijan0034 <damijan0034@gmail.com>
2023-03-08 06:49:49 -0800

    add card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {FiThumbsUp} from 'react-icons/fi'
 
 export default function Card({result}) {
   return (
@@ -12,7 +13,7 @@ export default function Card({result}) {
           }`}
           width={500}
           height={300}
-          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+          className=" sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
           style={{
             maxWidth: "100%",
             height: "auto",
@@ -21,6 +22,14 @@ export default function Card({result}) {
           blurDataURL="/spinner.svg"
           alt="image is not available"
         ></Image>
+        <div className='p-2'>
+            <p className='text-md'>{result.overview}</p>
+            <h2 className='text-lg font-bold'>{result.title || result.name}</h2>
+            <p className='flex items-center'>{result.release_date || result.first_aire_date}
+            <FiThumbsUp className='h-5 ml-1 mr-2' />{result.vote_count }
+            </p>
+            
+        </div>
       </Link>
       </div>
   )
